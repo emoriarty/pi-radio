eb3a32c92aa68b3630bb154259ec730984006b58
Enrique <emoriarty81@gmail.com>
2020-12-06 01:26:50 +0100

    chore: parsing stations

--- browser.py
+++ browser.py
@@ -2,9 +2,10 @@ import os
 import requests
 from dotenv import load_dotenv
 from lxml import etree as ET
-from xml_parser import parse_dir
+from xml_parser import parse_dir, parse_station
 
 load_dotenv()
+
 
 class Browser:
     HOST = os.getenv('YCAST_HOST')
@@ -12,7 +13,9 @@ class Browser:
     URL = HOST + ':' + str(PORT)
 
     def __init__(self):
-        self.parser = ET.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
+        self.parser = ET.XMLParser(ns_clean=True,
+                                   recover=True,
+                                   encoding='utf-8')
 
         req = requests.get(Browser.URL)
         root = ET.fromstring(req.text.encode('utf-8'), self.parser)
@@ -27,58 +30,83 @@ class Browser:
         self.countries = {}
         self.languages = {}
         self.most_popular = {}
-        self.fetch_lists(cache = False)
+        self.fetch_lists(cache=False)
 
-
-    def fetch_lists(self, cache = True):
-        if cache == True:
+    def fetch_lists(self, cache=True):
+        if cache is True:
             return self.lists
 
         req = requests.get(self.lists_url)
         titles, urls, counts = parse_dir(req.text)
 
-        for n in range(len(urls)):
-            type = titles[n].lower().replace(" ", "_")
-            print(type + ' (' + counts[n] + '): ' +  urls[n])
-            self.lists[type] = {
-                "title": titles[n],
-                "url": urls[n],
-                "count": counts[n]
+        for i in range(len(urls)):
+            typ = titles[i].lower().replace(" ", "_")
+            print(typ + ' (' + counts[i] + '): ' + urls[i])
+            self.lists[typ] = {
+                "title": titles[i],
+                "url": urls[i],
+                "count": counts[i]
             }
 
         return self.lists
 
-    def fetch_genres(self, cache = True):
-        if cache == True:
+    def fetch_genres(self, cache=True):
+        if cache is True:
             return self.genres
 
-        req = requests.get(self.lists["genres"]["url"])
-        titles, urls, counts = parse_dir(req.text)
+        self.genres = fetch_and_parse_dir(self.lists["genres"]["url"])
+        #return self.genres
 
-        for n in range(len(urls)):
-            genre = titles[n].lower().replace(" ", "_")
-            print(genre + ' (' + counts[n] + '): ' +  urls[n])
-            self.genres[genre] = {
-                "title": titles[n],
-                "url": urls[n],
-                "count": counts[n]
-            }
-        return self.genres
+    def fetch_countries(self, cache=True):
+        if cache is True:
+            return self.countries
 
-# req_popular = requests.get(stations_lists_urls[3])
-# popular = ET.fromstring(req_popular.text.encode('utf-8'), parser)
-# 
-# popular_titles = popular.xpath('//StationName/text()')
-# popular_urls = popular.xpath('//StationUrl/text()')
-# popular_count = popular.xpath('//StationDesc/text()')
-# 
-# print(popular_titles)
-#
-#languagesUrl = ''
-#for child in lists[2]:
-#    if child.tag == 'UrlDir':
-#        print(child.text)
-#        languagesUrl = child.text
-#
-#languages = requests.get(languagesUrl).text
-#print(languages)
+        self.countries = fetch_and_parse_dir(self.lists["countries"]["url"])
+        #return self.countries
+
+    def fetch_languages(self, cache=True):
+        if cache is True:
+            return self.languages
+
+        self.languages = fetch_and_parse_dir(self.lists["languages"]["url"])
+        #return self.languages
+
+    def fetch_most_popular(self, cache=True):
+        if cache is True:
+            return self.most_popular
+
+        print(self.lists["most_popular"]["url"])
+        self.most_popular = fetch_and_parse_station(
+            self.lists["most_popular"]["url"])
+        #return self.most_popular
+
+
+def fetch_and_parse_dir(url):
+    req = requests.get(url)
+    titles, urls, counts = parse_dir(req.text)
+    dirs = {}
+
+    for n in range(len(urls)):
+        dire = titles[n].lower().replace(" ", "_")
+        print(dire + ' (' + counts[n] + '): ' + urls[n])
+        dirs[dire] = {"title": titles[n], "url": urls[n], "count": counts[n]}
+
+    return dirs
+
+
+def fetch_and_parse_station(url):
+    req = requests.get(url)
+    names, urls, logos, mimes, bandrates = parse_station(req.text)
+    stations = []
+
+    for i in range(len(urls)):
+        print(names[i] + ' (' + urls[i] + '): ' + mimes[i])
+        stations.append({
+            "name": names[i],
+            "url": urls[i],
+            "logo": logos[i],
+            "mime": mimes[i],
+            "bandrate": bandrates[i],
+        })
+
+    return stations
--- xml_parser.py
+++ xml_parser.py
@@ -2,6 +2,7 @@ from lxml import etree as ET
 
 parser = ET.XMLParser(ns_clean=True, recover=True, encoding='utf-8')
 
+
 def parse_dir(raw):
     doc = ET.fromstring(raw.encode('utf-8'), parser)
 
@@ -10,3 +11,15 @@ def parse_dir(raw):
     counts = doc.xpath('//DirCount/text()')
 
     return (titles, urls, counts)
+
+
+def parse_station(raw):
+    doc = ET.fromstring(raw.encode('utf-8'), parser)
+
+    names = doc.xpath('//StationName/text()')
+    urls = doc.xpath('//StationUrl/text()')
+    logos = doc.xpath('//Logo/text()')
+    mimes = doc.xpath('//StationMime/text()')
+    bandrates = doc.xpath('//StationBandWidth/text()')
+
+    return (names, urls, logos, mimes, bandrates)
