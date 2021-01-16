af5f2d25ffc404a442d77269e809de48d4187570
enrique arias <emoriarty81@gmail.com>
2021-01-17 00:14:14 +0100

    create lists

--- radiopi/my_stations.py
+++ radiopi/my_stations.py
@@ -1,15 +1,21 @@
 import yaml
 from .settings import MY_STATIONS
 
+
 def get_lists():
     with open(MY_STATIONS) as file:
         data = yaml.load(file, Loader=yaml.FullLoader)
         return data.keys()
 
+
 def get_stations(list_name):
     with open(MY_STATIONS) as file:
-        stations = yaml.load(file, Loader=yaml.FullLoader)[list_name]
-        return list(zip(stations.keys(), stations.values()))
+        lists = yaml.load(file, Loader=yaml.FullLoader)
+        if list_name in lists:
+            stations = lists[list_name]
+            return list(zip(stations.keys(), stations.values()))
+        return []
+
 
 def save_station(list_name, *station):
     lists = {}
@@ -17,7 +23,10 @@ def save_station(list_name, *station):
     with open(MY_STATIONS) as file:
         lists = yaml.load(file, Loader=yaml.FullLoader)
 
-    lists[list_name][station[0]] = station[1]
+    if list_name in lists:
+        lists[list_name][station[0]] = station[1]
+    else:
+        lists[list_name] = {station[0]: station[1]}
 
     with open(MY_STATIONS, 'w') as file:
         yaml.dump(lists, file)
