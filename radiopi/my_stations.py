fe1c4d25204538bcf598058ebd5b7a0c685ad768
enrique arias <emoriarty81@gmail.com>
2021-01-26 23:44:21 +0100

    chore: MyStations class

--- radiopi/my_stations.py
+++ radiopi/my_stations.py
@@ -2,31 +2,35 @@ import yaml
 from .settings import MY_STATIONS
 
 
-def get_lists():
-    with open(MY_STATIONS) as file:
-        data = yaml.load(file, Loader=yaml.FullLoader)
-        return data.keys()
+class MyStations():
+    @staticmethod
+    def get_lists():
+        with open(MY_STATIONS) as file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+            return data.keys()
 
 
-def get_stations(list_name):
-    with open(MY_STATIONS) as file:
-        lists = yaml.load(file, Loader=yaml.FullLoader)
+    @staticmethod
+    def get_stations(list_name):
+        with open(MY_STATIONS) as file:
+            lists = yaml.load(file, Loader=yaml.FullLoader)
+            if list_name in lists:
+                stations = lists[list_name]
+                return list(zip(stations.keys(), stations.values()))
+            return []
+
+
+    @staticmethod
+    def save_station(list_name, *station):
+        lists = {}
+
+        with open(MY_STATIONS) as file:
+            lists = yaml.load(file, Loader=yaml.FullLoader)
+
         if list_name in lists:
-            stations = lists[list_name]
-            return list(zip(stations.keys(), stations.values()))
-        return []
+            lists[list_name][station[0]] = station[1]
+        else:
+            lists[list_name] = {station[0]: station[1]}
 
-
-def save_station(list_name, *station):
-    lists = {}
-
-    with open(MY_STATIONS) as file:
-        lists = yaml.load(file, Loader=yaml.FullLoader)
-
-    if list_name in lists:
-        lists[list_name][station[0]] = station[1]
-    else:
-        lists[list_name] = {station[0]: station[1]}
-
-    with open(MY_STATIONS, 'w') as file:
-        yaml.dump(lists, file)
+        with open(MY_STATIONS, 'w') as file:
+            yaml.dump(lists, file)
