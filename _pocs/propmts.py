b5cf10a16c571b5738e174b02572cef652cb6507
enrique arias <emoriarty81@gmail.com>
2021-01-26 23:46:24 +0100

    chore: clean useless files

--- _pocs/propmts.py
+++
@@ -1,8 +0,0 @@
-from prompt_toolkit import PromptSession
-
-session = PromptSession()
-
-text1 = session.prompt()
-text2 = session.prompt()
-
-
--- _pocs/simple_app.py
+++
@@ -1,4 +0,0 @@
-from prompt_toolkit import Application
-
-app = Application(full_screen=True)
-app.run()
