a1fb0bb009de682efa2f88f0d0196a3441ad7a8a
enrique arias <emoriarty81@gmail.com>
2021-01-27 00:04:40 +0100

    chore: env vars from app config folder

--- radiopi/settings.py
+++ radiopi/settings.py
@@ -1,11 +1,13 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv as _load_dotenv, find_dotenv as _find_dotenv
-
-_load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+from dotenv import load_dotenv as _load_dotenv
 
 APP_FOLDER = 'radiopi'
 CONFIG_PATH = os.path.join(Path.home(), '.config', APP_FOLDER)
 MY_STATIONS = os.path.join(CONFIG_PATH, 'stations.yml')
+
+_load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+_load_dotenv(os.path.join(CONFIG_PATH, '.env'))
+
 # create config folder
 Path(CONFIG_PATH).mkdir(parents=True, exist_ok=True)
