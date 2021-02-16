aa012b81ce25363ec09fd1e5ca4aaf8554405aef
enrique arias <emoriarty81@gmail.com>
2021-02-17 00:46:01 +0100

    Stations by country

--- radiopi_web/www/scripts/custom-tags/views/radio-country-view.js
+++ radiopi_web/www/scripts/custom-tags/views/radio-country-view.js
@@ -1,9 +1,11 @@
 import { LitElement, html, css } from "https://jspm.dev/lit-element@2";
+import { fetchStationsByCountry } from "../../radio-browser-api.js";
 
 export class RadioCountryView extends LitElement {
   static get properties() {
     return {
       country: { type: String },
+      stations: { type: Array },
     };
   }
 
@@ -24,12 +26,40 @@ export class RadioCountryView extends LitElement {
     `;
   }
 
-  // createRenderRoot() {
-  //   return this;
-  // }
+  constructor() {
+    super();
+    this.stations = [];
+    this.renderStation = this.renderStation.bind(this);
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((_, propName) => {
+      if (propName == "country" && this.country) {
+        fetchStationsByCountry(this.country).then((data) => {
+          this.stations = data;
+        });
+      }
+    });
+  }
 
   render() {
-    return html` <h2>Radio Countries &gt; ${this.country}</h2> `;
+    console.log(this.country);
+    return html`
+      <h2>Radio Countries &gt; ${this.country}</h2>
+      <stations-grid-list>
+        ${this.stations.map(this.renderStation)}
+      </stations-grid-list>
+    `;
+  }
+
+  renderStation(station) {
+    return html`
+      <station-card
+        img="${station.favicon}"
+        title="${station.name}"
+        id="${station.changeuuid}"
+      ></station-card>
+    `;
   }
 }
 
